6ca4087fd47d1618e8538bcdbdcc183f83e54d6c
Overlord_Zoltron <jmarsenault@email.neit.edu>
2016-12-01 19:16:39 -0500

    Added comments to initials.py

--- initials.py
+++ initials.py
@@ -1,8 +1,12 @@
 def get_initials(fullname):
+    #split the full name into chunks
     split_name = fullname.split()
     initials = ""
+    #go through the split name
     for i in range(len(split_name)):
+        #for every split select the first letter
         initials += split_name[i][0]
+    #return the accumulated initials in uppercase
     return initials.upper()
 
 #answer = get_initials("Daniel Day Lewis")
